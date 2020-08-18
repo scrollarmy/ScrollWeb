79436860d736e3d4276af09dc39e26d730b31d6e
VanneyFranco <francovanney83@gmail.com>
2020-08-18 20:35:57 -0300

    Navbar resposive, mas o menos

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+(function($) { 
+    $(document).ready(function(){   //Navbar mistica con fadein        
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 500) {
+                $('.navbar').fadeIn(500);
+            } else {
+                $('.navbar').fadeOut(500);
+            }
+        });
+    });
+})(jQuery);
+
+$(document).ready(function(){ //Menu responsive
+    $('.hamburguesa').click(function(){
+        $('.navbarcolapse').toggleClass("colapse")
+    })
+});
